993d9f52b71d261b5e546727f24135c2e4cb0be6
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-13 15:28:12 -0300

    Finished lesson 54. My solution was actually pretty close the what was done, with the exception I hadn't built a custom widget, and insted put everything on the main.

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
 }
-
 
 // void main() {
 //   runApp(
