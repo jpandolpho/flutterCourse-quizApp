f8aca3b45dbbe2046926f2d30f048f969143fe5d
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-13 16:05:46 -0300

    Finished lesson 57. Started preparing the stateful part of the app. Also removed the comment with the original build I made, since it can be checked through the commits in this project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz(),
   );
 }
-
-// void main() {
-//   runApp(
-//     MaterialApp(
-//       home: Scaffold(
-//         body: Container(
-//           decoration: const BoxDecoration(
-//             gradient: LinearGradient(
-//               colors: [
-//                 Colors.deepPurple,
-//                 Color.fromARGB(255, 65, 30, 125),
-//               ],
-//               begin: Alignment.topLeft,
-//               end: Alignment.bottomRight,
-//             ),
-//           ),
-//           child: Center(
-//             child: Column(
-//               mainAxisSize: MainAxisSize.min,
-//               children: [
-//                 Image.asset(
-//                   'assets/images/quiz-logo.png',
-//                   width: 200,
-//                 ),
-//                 const SizedBox(height: 20),
-//                 const Text(
-//                   'Learn flutter the fun way!',
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontSize: 28,
-//                   ),
-//                 ),
-//                 const SizedBox(height: 20),
-//                 OutlinedButton(
-//                   onPressed: () => {},
-//                   child: const Text(
-//                     "Start Quiz",
-//                     style: TextStyle(
-//                       color: Colors.white,
-//                       fontSize: 14,
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     ),
-//   );
-// }
