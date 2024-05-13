fd768e6ba1a3af6c03da3a912af5a406ba0c213a
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-13 14:44:50 -0300

    Started a new section of the course and finished lesson 52, where we were instructed to try to build on our own the initial version of the app that's going to be developed. Think I did a pretty ok job.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Quiz App
+
+This Flutter project was made following the course "Flutter & Dart - The Complete Guide [2024 Edition]", available in https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/.
+
+## Description
+
+In the third section of the course, the objective was diving deeper into the languages and learning more about some flutter widgets.
+
+To accomplish that, a Quiz App was created, where the user would answer questions about the flutter language, picking one of four possible answer. After answering all the questions, the app shows a summary of how many questions you got right, and shows the correct answer to the questions you got wrong. You can also retry the quiz after doing it.
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.deepPurple,
+                Color.fromARGB(255, 65, 30, 125),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 200,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Learn flutter the fun way!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                OutlinedButton(
+                  onPressed: () => {},
+                  child: const Text(
+                    "Start Quiz",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
