d80a1a2454439c589d9720753adc43274f0af481
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-13 14:57:59 -0300

    Finished lesson 53. Started implementing the solution with the teacher, but kept what I originally did as a comment in the end of the file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.deepPurple,
-                Color.fromARGB(255, 65, 30, 125),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  width: 200,
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Learn flutter the fun way!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                OutlinedButton(
-                  onPressed: () => {},
-                  child: const Text(
-                    "Start Quiz",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+        body: StartScreen(),
       ),
     ),
   );
 }
+
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         body: Container(
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//               colors: [
+//                 Colors.deepPurple,
+//                 Color.fromARGB(255, 65, 30, 125),
+//               ],
+//               begin: Alignment.topLeft,
+//               end: Alignment.bottomRight,
+//             ),
+//           ),
+//           child: Center(
+//             child: Column(
+//               mainAxisSize: MainAxisSize.min,
+//               children: [
+//                 Image.asset(
+//                   'assets/images/quiz-logo.png',
+//                   width: 200,
+//                 ),
+//                 const SizedBox(height: 20),
+//                 const Text(
+//                   'Learn flutter the fun way!',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 28,
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 OutlinedButton(
+//                   onPressed: () => {},
+//                   child: const Text(
+//                     "Start Quiz",
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 14,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
