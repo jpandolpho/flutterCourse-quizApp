ffe2a4643b6c8c405f611b709394d4f4e0446015
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-14 16:20:58 -0300

    Finished lessons 69-70. Sad that my emoji didn't work in the last commit. Eliminated some code duplication mapping the list and then spreading it.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,33 +14,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity, //alternativa para o center
       child: Column(
         mainAxisAlignment:
             MainAxisAlignment.center, //meio que nem uma flexbox de css
         children: [
-          const Text(
-            "question",
-            style: TextStyle(color: Colors.white),
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            answerText: 'answerText1',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answerText2',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answerText3',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'answerText4',
-            onTap: () {},
-          ),
+          ...currentQuestion.answers.map((answer) {
+            //... é um operador que transforma uma lista/iterável em valores separados por vírgula.
+            //então neste caso, ele pega os AnswerButton que estão numa lista, e transpõe eles pra,
+            //ao invés de ser uma lista dentro de children, serem objetos dentro de children
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
         ],
       ),
     );
