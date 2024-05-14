b6d2efae0408f64d5a4e2cc4987ac0286a8c32c4
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-14 16:31:11 -0300

    Finished lessons 71-72. Did some styling and shuffled the answers in the question screen.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -17,22 +17,27 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity, //alternativa para o center
-      child: Column(
-        mainAxisAlignment:
-            MainAxisAlignment.center, //meio que nem uma flexbox de css
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer) {
-            //... é um operador que transforma uma lista/iterável em valores separados por vírgula.
-            //então neste caso, ele pega os AnswerButton que estão numa lista, e transpõe eles pra,
-            //ao invés de ser uma lista dentro de children, serem objetos dentro de children
-            return AnswerButton(answerText: answer, onTap: () {});
-          }),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, //meio que nem uma flexbox de css
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              //... é um operador que transforma uma lista/iterável em valores separados por vírgula.
+              //então neste caso, ele pega os AnswerButton que estão numa lista, e transpõe eles pra,
+              //ao invés de ser uma lista dentro de children, serem objetos dentro de children
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
       ),
     );
   }
