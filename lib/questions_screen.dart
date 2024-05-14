85717a97e42b7de3e5f39bdf900c38c4c3e0255d
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-14 16:04:07 -0300

    Finished lesson 67-68. The question screen is mostly done, with exception that both question and answers are still placeholders, and that the button don't do anything :grinning_face_with_sweat:

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,35 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('question screen');
+    return SizedBox(
+      width: double.infinity, //alternativa para o center
+      child: Column(
+        mainAxisAlignment:
+            MainAxisAlignment.center, //meio que nem uma flexbox de css
+        children: [
+          const Text(
+            "question",
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'answerText1',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'answerText2',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'answerText3',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'answerText4',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
