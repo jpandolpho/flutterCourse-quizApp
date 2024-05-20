f18ee1fe2b4ce859ed422f92ec12a459841123af
João Pedro Andolpho <joaopedrodealmeidaandolpho@gmail.com>
2024-05-20 14:51:23 -0300

    Finished lesson 77. The quiz now loops, no results screen yet.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:quiz_app/data/questions.dart";
 import "package:quiz_app/questions_screen.dart";
 import "package:quiz_app/start_screen.dart";
 
@@ -12,7 +13,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   Widget? activeScreen;
 
   /*
@@ -39,6 +40,12 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = StartScreen(switchScreen);
+      });
+    }
   }
 
   @override
